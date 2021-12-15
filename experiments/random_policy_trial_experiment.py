513b0bc404faa74146b82e2f0603f87d8475d11b
Pablo Kvitca <pablokvitca@gmail.com>
2021-12-15 08:09:52 -0300

    update sarsa plot

--- experiments/random_policy_trial_experiment.py
+++ experiments/random_policy_trial_experiment.py
@@ -5,63 +5,67 @@ from policies.RandomPolicyPlaceMoveEqual import RandomPolicyPlaceMoveEqual
 from tak_env.TakEnvironment import TakEnvironment
 from tak_env.TakPlayer import TakPlayer
 
-# register(id=TakEnvironment.ENV_NAME, entry_point="tak_env:TakEnvironment")
+if __name__ == '__main__':
+    # register(id=TakEnvironment.ENV_NAME, entry_point="tak_env:TakEnvironment")
 
-# env = make(TakEnvironment.ENV_NAME, board_size=3)
-env = TakEnvironment(board_size=5)
+    # env = make(TakEnvironment.ENV_NAME, board_size=3)
+    env = TakEnvironment(board_size=5)
 
-agent_white_player = TakPlayerAgent(TakPlayer.WHITE, policy=RandomPolicyPlaceMoveEqual())
-agent_black_player = TakPlayerAgent(TakPlayer.BLACK, policy=RandomPolicyPlaceMoveEqual())
+    agent_white_player = TakPlayerAgent(TakPlayer.WHITE, policy=RandomPolicyPlaceMoveEqual())
+    agent_black_player = TakPlayerAgent(TakPlayer.BLACK, policy=RandomPolicyPlaceMoveEqual())
 
-info = {'ended_with_path': False}
-
-
-def log(log_pre, s):
-    return log_pre + s + '\n'
+    info = {'ended_with_path': False}
 
 
-log_str = "Nothing here"
+    def log(log_pre, s):
+        return log_pre + s + '\n'
 
-for _ in trange(10000):
-    state = env.reset()
-    log_str = ""
 
-    done = False
-    while not done:
-        action = agent_white_player.select_action(state)
-        state, reward, done, info = env.step(action)
-        log_str = log(log_str, f"WHITE ACTION: {action} -> {reward}, {done}")
-        # env.render()
-        if done:
+    log_str = "Nothing here"
+
+    for _ in trange(1):
+        state = env.reset()
+        log_str = ""
+
+        done = False
+        while not done:
+            action = agent_white_player.select_action(state)
+            state, reward, done, info = env.step(action)
+            log_str = log(log_str, f"WHITE ACTION: {action} -> {reward}, {done}")
+            if env.state.board.as_3d_matrix()[1] > 1:
+                env.render()
+            if done:
+                break
+
+            action = agent_black_player.select_action(state)
+            state, reward, done, info = env.step(action)
+            log_str = log(log_str, f"BLACK ACTION: {action} -> {reward}, {done}")
+            if env.state.board.as_3d_matrix()[1] > 1:
+                env.render()
+            if done:
+                break
+
+        if env.state.board.as_3d_matrix()[1] > 1:
+            env.render()
+
+        log_str = log(log_str, "GAME END INFO:")
+        log_str = log(log_str, f"""
+        GAME END INFO:
+            Winning Player: {info['winning_player']}
+            Ended with path: {info['ended_with_path']}
+            Ended with no pieces left: {info['ended_with_no_pieces_left']}
+            Ended with no spaces left: {info['ended_with_no_spaces_left']}
+            WHITE: Remaining Pieces: {info['white_pieces_available']} Remaining Capstone: {info['white_capstone_available']}
+            BLACK: Remaining Pieces: {info['black_pieces_available']} Remaining Capstone: {info['black_capstone_available']}
+        """)
+
+        if info['ended_with_path']:
             break
 
-        action = agent_black_player.select_action(state)
-        state, reward, done, info = env.step(action)
-        log_str = log(log_str, f"BLACK ACTION: {action} -> {reward}, {done}")
-        # env.render()
-        if done:
-            break
+    print(log_str)
 
-    # env.render()
+    env.render()
 
-    log_str = log(log_str, "GAME END INFO:")
-    log_str = log(log_str, f"""
-    GAME END INFO:
-        Winning Player: {info['winning_player']}
-        Ended with path: {info['ended_with_path']}
-        Ended with no pieces left: {info['ended_with_no_pieces_left']}
-        Ended with no spaces left: {info['ended_with_no_spaces_left']}
-        WHITE: Remaining Pieces: {info['white_pieces_available']} Remaining Capstone: {info['white_capstone_available']}
-        BLACK: Remaining Pieces: {info['black_pieces_available']} Remaining Capstone: {info['black_capstone_available']}
-    """)
+    env.state.board.as_3d_matrix()[0].tofile("savedboard_1.txt")
 
-    if info['ended_with_path']:
-        break
-
-print(log_str)
-
-env.render()
-
-env.state.board.as_3d_matrix()[0].tofile("savedboard_1.txt")
-
-env.close()
+    env.close()
